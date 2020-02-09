f77fe4a9b0be22cd8cccae2c22ea7a089f2cb30b
wellingtonorosa <w_oliveirarosa@hotmail.com>
2020-02-09 16:33:12 -0300

    Seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -42,7 +42,7 @@ public boolean equals(Object obj) {
 	if (getClass() != obj.getClass())
 		return false;
 	Seller other = (Seller) obj;
-	if (id == null) {
+	if (id == null) { 
 		if (other.id != null)
 			return false;
 	} else if (!id.equals(other.id))
